0b05109ec252e8eee0d6b62a65bb35bcb896dc01
Chibuzorchi <chinonsochibuzor2277@gmail.com>
2022-04-05 10:22:21 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WellaHealth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        /* string myName = "Chinonso Ebelema Kinsmen";
+            long myNumber = 09076130910;
+            int myAge = 27;
+            double nairaToDollar = 570.67;
+            int nairaToDollarInt = (int)nairaToDollar;
+            
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(myName);
+            Console.WriteLine(myName);
+            Console.WriteLine(myNumber);
+            Console.WriteLine(myAge);
+            Console.WriteLine(nairaToDollarInt);
+        /* Console.WriteLine(nairaToDollar);
+
+
+
+
+        }
+    }
+}
